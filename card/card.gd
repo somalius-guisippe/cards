5b70c6d9c64da408bcc70402e96dcd9179a25026
joseph <joe.moronuki@gmail.com>
2025-04-13 01:04:03 -0600

    init

---
+++ card/card.gd
@@ -0,0 +1,41 @@
+@tool
+class_name Card
+extends Node2D
+
+enum Suit {
+  Spade,
+  Heart,
+  Diamond,
+  Club
+}
+
+@export_group("Card")
+
+@export var suit: Suit = Suit.Spade:
+	set = _set_suit
+	
+@export_range(1, 13) var rank: int = 1:
+	set = _set_rank
+
+@onready var _sprite: AnimatedSprite2D = $Sprite
+
+signal change
+
+func _ready():
+	change.connect(_on_change)
+	_pick_animation_frame()
+
+func _pick_animation_frame():
+	_sprite.frame = suit * 13 + rank - 1
+
+func _on_change():
+	if is_node_ready():
+		_pick_animation_frame()
+
+func _set_suit(x: Suit):
+	suit = x
+	change.emit()
+
+func _set_rank(x: int):
+	rank = x
+	change.emit()
---
+++ tableau/tableau.gd
@@ -0,0 +1,3 @@
+@tool
+class_name Deck
+extends Node2D
