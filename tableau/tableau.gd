81249b205287a3207a53d9757d282c5c287d9a27
joseph <joe.moronuki@gmail.com>
2025-05-03 11:14:19 -0600

    randomize

--- tableau/tableau.gd
+++ tableau/tableau.gd
@@ -3,11 +3,18 @@ class_name Tableau
 extends Node2D
 
 func _ready():
+	var deck = []
 	for suit in range(0,4):
 		for rank in range(1,13 + 1):
 			var card = $FirstCard.duplicate()
+			deck.append(card)
 			add_child(card)
 			card.rank = rank
 			card.suit = suit
-			card.position = Vector2(100 + 50 * rank, 100 + 30 * rank + 50 * suit)
+			#card.position = Vector2(100 + 50 * rank, 100 + 30 * rank + 50 * suit)
+	deck.shuffle()
+	for i in range(deck.size()):
+		var card = deck[i]
+		card.position = Vector2(100 + 50 * i, 100 + 30 * i + 50 * i)
+		move_child(card, i)
 	
