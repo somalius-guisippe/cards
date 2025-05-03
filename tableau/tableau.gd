4dca74d812dfe8dc1cd720792c6db79cede6c7a3
joseph <joe.moronuki@gmail.com>
2025-05-03 11:40:14 -0600

    everything in order.

--- tableau/tableau.gd
+++ tableau/tableau.gd
@@ -2,19 +2,27 @@
 class_name Tableau
 extends Node2D
 
+var normal_cells
+var cards
+
 func _ready():
 	var deck = []
+	normal_cells = $normalCells.get_children()
+	cards = $cards
 	for suit in range(0,4):
 		for rank in range(1,13 + 1):
 			var card = $FirstCard.duplicate()
 			deck.append(card)
-			add_child(card)
+			cards.add_child(card)
 			card.rank = rank
 			card.suit = suit
 			#card.position = Vector2(100 + 50 * rank, 100 + 30 * rank + 50 * suit)
 	deck.shuffle()
 	for i in range(deck.size()):
+		var row = i/8
+		var column = i%8
 		var card = deck[i]
-		card.position = Vector2(100 + 50 * i, 100 + 30 * i + 50 * i)
-		move_child(card, i)
+		var cell = normal_cells[column]
+		card.position = Vector2(cell.position.x, cell.position.y + row * 30)
+		cards.move_child(card, i)
 	
