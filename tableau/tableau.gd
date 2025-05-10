dd2f3a5fc8613fd3d8045748ebdebbbeba835e9f
joseph <joe.moronuki@gmail.com>
2025-05-10 13:14:20 -0600

    you can move cards now!

--- tableau/tableau.gd
+++ tableau/tableau.gd
@@ -4,6 +4,15 @@ extends Node2D
 
 var normal_cells
 var cards
+#the card we're moving at the moment
+var moving_card
+#where the mouse is at the beggining of clicking the card
+var movement_start
+#movement start for the card
+var card_start
+
+# Eight lists of cards, arranged from the bottom of the pile to the top
+var columns
 
 func area_to_card(object):
 	var card := object.collider.get_parent() as Card
@@ -22,37 +31,44 @@ func _input(event):
 	x.collide_with_areas = true
 
 	var object
-	var mouse_event := event as InputEventMouse
-	if event.is_pressed() and mouse_event != null: 
-		var objects_clicked = get_world_2d().direct_space_state.intersect_point(x, 10)
-		var cards_clicked = objects_clicked.map(area_to_card).filter(func(x): return x != null)
-		var top_card = find_top_card(cards_clicked)
-		if top_card != null:
-			print(top_card.rank)
-			
-		#
-		#for object in objects_clicked:
-			#var card := object.get_parent() as Card
-			#if card != null:
-				#yield card
-		#
-		#if objects_clicked.size() > 0:
-			#object = objects_clicked[-1].collider
-			#
-			#
-			#var card := object.get_parent() as Card
-			#if card != null:
-				#var all_cards = $cards.get_children()
-				#all_cards.reverse()
-				#for c in all_cards:
-					#
-					#print(card.rank)
+	var button_event := event as InputEventMouseButton
+	var motion_event := event as InputEventMouseMotion
+	if motion_event != null:
+		if moving_card != null:
+			moving_card.position = card_start + ( event.position - movement_start )
+	if button_event != null:
+		if event.is_pressed(): 
+			var objects_clicked = get_world_2d().direct_space_state.intersect_point(x, 10)
+			var cards_clicked = objects_clicked.map(area_to_card).filter(func(x): return x != null)
+			var picked_card = find_top_card(cards_clicked)
+			if picked_card != null:
+				if (is_at_top_of_column(picked_card)):
+					moving_card = picked_card
+					movement_start = event.position
+					card_start = picked_card.position
+		if event.is_released():
+			print(event)
+			if moving_card != null:
+				moving_card.position = card_start
+				moving_card = null
+				movement_start = null
+				card_start = null
 
+func is_at_top_of_column(card):
+	for column in columns:
+		if card == column[-1]:
+			return true
+	return false
 
 func _ready():
 	var deck = []
 	normal_cells = $normalCells.get_children()
 	cards = $cards
+	
+	columns = []
+	for i in range(0,8):
+		columns.append([])
+	
 	for suit in range(0,4):
 		for rank in range(1,13 + 1):
 			var card = $FirstCard.duplicate()
@@ -67,6 +83,7 @@ func _ready():
 		var column = i%8
 		var card = deck[i]
 		var cell = normal_cells[column]
+		columns[column].append(card)
 		card.position = Vector2(cell.position.x, cell.position.y + row * 30)
 		cards.move_child(card, i)
 	
